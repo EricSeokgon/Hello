8c044eb6e1cdf839f4e7d5afd78abe4f26d19fcd
sklee <hadesleeq@gmail.com>
2018-12-07 17:29:46 +0900

    버튼 변경

--- app/src/main/java/com/tistory/hadeslee/hello/MainActivity.java
+++ app/src/main/java/com/tistory/hadeslee/hello/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tistory.hadeslee.hello;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View view) {
+        Toast.makeText(getApplicationContext(), "버튼이 눌렀어요.", Toast.LENGTH_LONG).show();
     }
 }
