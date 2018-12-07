fcfbeebd196fd6c0320f8ac97c72c5b1198cf2dd
sklee <hadesleeq@gmail.com>
2018-12-07 17:38:06 +0900

    토스트 메시지, 인텐트 웹앱 추가

--- app/src/main/java/com/tistory/hadeslee/hello/MainActivity.java
+++ app/src/main/java/com/tistory/hadeslee/hello/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tistory.hadeslee.hello;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButton1Clicked(View view) {
         Toast.makeText(getApplicationContext(), "버튼이 눌렀어요.", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
     }
 }
