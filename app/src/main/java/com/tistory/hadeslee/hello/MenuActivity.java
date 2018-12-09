37d653ef969c7e265f40c28cc559109851e93fbe
hadesyi <leesk55@gmail.com>
2018-12-10 03:34:23 +0900

    인탠트 네이버 접속하기 전화걸기

---
+++ app/src/main/java/com/tistory/hadeslee/hello/MenuActivity.java
@@ -0,0 +1,19 @@
+package com.tistory.hadeslee.hello;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void onBackButtonClicked(View v) {
+        Toast.makeText(getApplicationContext(),"돌아가기 버튼을 눌렀어요",Toast.LENGTH_LONG).show();
+    }
+}
